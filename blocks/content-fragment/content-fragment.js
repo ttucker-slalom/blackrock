76b88d853e14b1518256a4550c2af2d67f7c5818
smruti.mohapatra <smruti.mohapatra@greatdentalplans.com>
2024-04-23 17:22:09 -0500

    fixed lint issues

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -1,31 +1,3 @@
-// import ffetch from '../../scripts/ffetch.js';
-
-// export function getOrigin() {
-//    return window.location.href === 'about:srcdoc' ? window.parent.location.origin : window.location.origin;
-//  }
-
-// /* eslint-disable no-underscore-dangle */
-// export default async function decorate(block) {
-  
-//    const offerpath = block.querySelector(':scope div:nth-child(1) > div a').innerHTML.trim();
-
-//     console.log( offerpath);
-    
-//    const insightsUrl = new URL('https://publish-p11802-e92257.adobeaemcloud.com/graphql/execute.json/aem-demo-assets/adventure-by-path;adventurePath='+offerpath);
-//     //const insightsUrl = new URL('https://publish-p11802-e92257.adobeaemcloud.com/graphql/execute.json/aem-demo-assets/adventures-all');
-    
-
-
-//     //const insightsUrl = new URL('https://lifesciences.danaher.com/us/en/solutions-index.json', getOrigin());
-//    const insights = await ffetch(insightsUrl).first();
-//    let i = 0;
-//    for await (const insight of insights) {
-//      console.log(insight.adventureByPath.item._path);
-//    }
-
-   
-// }
-
 /* eslint-disable no-underscore-dangle */
 export default async function decorate(block) {
     const aempublishurl = 'https://publish-p11802-e92257.adobeaemcloud.com';
@@ -62,4 +34,3 @@ export default async function decorate(block) {
     </div>
   `;
   }
-  
