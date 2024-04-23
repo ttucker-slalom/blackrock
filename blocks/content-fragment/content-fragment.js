cd8d1652dffa541b40b60a5103519d0e0ea8f149
smruti.mohapatra <smruti.mohapatra@greatdentalplans.com>
2024-04-23 17:35:26 -0500

    lint issues

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -6,22 +6,22 @@ export default async function decorate(block) {
   const url = window.location && window.location.origin && window.location.origin.includes('author')
     ? `${aemauthorurl}${persistedquery};adventurePath=${insightsPath};`
     : `${aempublishurl}${persistedquery};adventurePath=${insightsPath};`;
-  const options = { credentials: 'include' }; 
+  const options = { credentials: 'include' };
   const cfReq = await fetch(url, options)
     .then((response) => response.json())
     .then((contentfragment) => {
-    let offer = '';
-        if (contentfragment.data) {
-          offer = contentfragment.data.adventureByPath.item;
-        }
-        return offer;
-      });
+      let offer = '';
+      if (contentfragment.data) {
+        offer = contentfragment.data.adventureByPath.item;
+      }
+      return offer;
+    });
   const itemId = `urn:aemconnection:${insightsPath}/jcr:content/data/master`;
   block.innerHTML = `
     <div class='creditcard' data-aue-resource=${itemId} data-aue-type="reference" data-aue-filter="cf">
         <div>
-            <p data-aue-prop="headline" data-aue-type="text" class='ccname'>${cfReq.title}</p>
-            <p data-aue-prop="headline" data-aue-type="text" class='ccname'>${cfReq.description.plaintext}</p>
+          <p data-aue-prop="headline" data-aue-type="text" class='ccname'>${cfReq.title}</p>
+          <p data-aue-prop="headline" data-aue-type="text" class='ccname'>${cfReq.description.plaintext}</p>
         </div>
         <div class='banner-logo'>
         </div>
