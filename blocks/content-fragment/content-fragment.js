05bb671b3396648175e46df8427eed7072e4ffad
smruti.mohapatra <smruti.mohapatra@greatdentalplans.com>
2024-04-23 18:10:31 -0500

    revert connection

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -10,14 +10,15 @@ export default async function decorate(block) {
   const cfReq = await fetch(url, options)
     .then((response) => response.json())
     .then((contentfragment) => {
-      let offer = '';
+      let insight = '';
       if (contentfragment.data) {
-        offer = contentfragment.data.adventureByPath.item;
+        insight = contentfragment.data.adventureByPath.item;
       }
-      return offer;
+      return insight;
     });
+  const itemId = `urn:aemconnection:${insightsPath}/jcr:content/data/master`;
   block.innerHTML = `
-    <div class='insights'>
+    <div class='insights' data-aue-resource=${itemId} data-aue-type="reference" data-aue-filter="cf">
         <div>
           <h2 data-aue-prop="headline" data-aue-type="text" class='insights-title'>${cfReq.title}</h2>
           <p data-aue-prop="headline" data-aue-type="text" class='insights-description'>${cfReq.description.plaintext}</p>
