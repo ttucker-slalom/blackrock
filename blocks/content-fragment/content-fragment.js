4d9c28c23fdee796273b668b5fb08626ddbd8f80
smruti.mohapatra <smruti.mohapatra@greatdentalplans.com>
2024-04-23 17:28:15 -0500

    lint issues

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -1,29 +1,23 @@
-/* eslint-disable no-underscore-dangle */
 export default async function decorate(block) {
-    const aempublishurl = 'https://publish-p11802-e92257.adobeaemcloud.com';
-    const aemauthorurl = 'https://author-p11802-e92257.adobeaemcloud.com';
-    const persistedquery = '/graphql/execute.json/aem-demo-assets/adventure-by-path';
-    const insightsPath = block.querySelector(':scope div:nth-child(1) > div a').innerHTML.trim();
-    //const variationname = block.querySelector(':scope div:nth-child(2) > div').innerHTML.trim();
-  
-    const url = window.location && window.location.origin && window.location.origin.includes('author')
-      ? `${aemauthorurl}${persistedquery};adventurePath=${insightsPath};`
-      : `${aempublishurl}${persistedquery};adventurePath=${insightsPath};`;
-    const options = { credentials: 'include' };
-  
-    const cfReq = await fetch(url, options)
-      .then((response) => response.json())
-      .then((contentfragment) => {
-        let offer = '';
+  const aempublishurl = 'https://publish-p11802-e92257.adobeaemcloud.com';
+  const aemauthorurl = 'https://author-p11802-e92257.adobeaemcloud.com';
+  const persistedquery = '/graphql/execute.json/aem-demo-assets/adventure-by-path';
+  const insightsPath = block.querySelector(':scope div:nth-child(1) > div a').innerHTML.trim();
+  const url = window.location && window.location.origin && window.location.origin.includes('author')
+    ? `${aemauthorurl}${persistedquery};adventurePath=${insightsPath};`
+    : `${aempublishurl}${persistedquery};adventurePath=${insightsPath};`;
+  const options = { credentials: 'include' }; 
+  const cfReq = await fetch(url, options)
+    .then((response) => response.json())
+    .then((contentfragment) => {
+    let offer = '';
         if (contentfragment.data) {
           offer = contentfragment.data.adventureByPath.item;
         }
         return offer;
       });
-  
-    const itemId = `urn:aemconnection:${insightsPath}/jcr:content/data/master`;
-  
-    block.innerHTML = `
+  const itemId = `urn:aemconnection:${insightsPath}/jcr:content/data/master`;
+  block.innerHTML = `
     <div class='creditcard' data-aue-resource=${itemId} data-aue-type="reference" data-aue-filter="cf">
         <div>
             <p data-aue-prop="headline" data-aue-type="text" class='ccname'>${cfReq.title}</p>
