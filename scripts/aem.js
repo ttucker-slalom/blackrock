81cd9be40dd0985c2c454c627281827188f7b956
Tracy Tucker <tracy.tucker@slalom.com>
2024-04-24 16:28:53 -0500

    Removes tertiary button from project

--- scripts/aem.js
+++ scripts/aem.js
@@ -410,17 +410,6 @@ function decorateButtons(element) {
           a.className = 'button secondary';
           twoup.classList.add('button-container');
         }
-        if (
-          up.childNodes.length === 1
-          && up.tagName === 'EM'
-          && twoup.childNodes.length === 1
-          && twoup.tagName === 'STRONG'
-          && twoup.parentElement.childNodes.length === 1
-          && twoup.parentElement.tagName === 'P'
-        ) {
-          a.className = 'button tertiary';
-          twoup.parentElement.classList.add('button-container');
-        }
       }
     }
   });
