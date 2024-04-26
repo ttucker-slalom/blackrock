1b57bfc06416bc9213ea52931ea8c3cf9a9a6f89
smruti.mohapatra <smruti.mohapatra@greatdentalplans.com>
2024-04-26 13:42:06 -0500

    indentation

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -196,62 +196,62 @@ let carouselId = 0;
 export default async function decorate(block) {
   iniFrame();
   if (!isIfrmae) {
-  carouselId += 1;
-  block.setAttribute('id', `carousel-${carouselId}`);
-  const rows = block.querySelectorAll(':scope > div');
-  const isSingleSlide = rows.length < 2;
+    carouselId += 1;
+    block.setAttribute('id', `carousel-${carouselId}`);
+    const rows = block.querySelectorAll(':scope > div');
+    const isSingleSlide = rows.length < 2;
 
-  const placeholders = await fetchPlaceholders();
+    const placeholders = await fetchPlaceholders();
 
-  block.setAttribute('role', 'region');
-  block.setAttribute('aria-roledescription', placeholders.carousel || 'Carousel');
+    block.setAttribute('role', 'region');
+    block.setAttribute('aria-roledescription', placeholders.carousel || 'Carousel');
 
-  const container = document.createElement('div');
-  container.classList.add('carousel-slides-container');
+    const container = document.createElement('div');
+    container.classList.add('carousel-slides-container');
 
-  const slidesWrapper = document.createElement('ul');
-  slidesWrapper.classList.add('carousel-slides');
-  block.prepend(slidesWrapper);
+    const slidesWrapper = document.createElement('ul');
+    slidesWrapper.classList.add('carousel-slides');
+    block.prepend(slidesWrapper);
 
-  let slideIndicators;
-  if (!isSingleSlide) {
-    const slideIndicatorsNav = document.createElement('nav');
-    slideIndicatorsNav.setAttribute('aria-label', placeholders.carouselSlideControls || 'Carousel Slide Controls');
-    slideIndicators = document.createElement('ol');
-    slideIndicators.classList.add('carousel-slide-indicators');
-    slideIndicatorsNav.append(slideIndicators);
-    block.append(slideIndicatorsNav);
+    let slideIndicators;
+    if (!isSingleSlide) {
+      const slideIndicatorsNav = document.createElement('nav');
+      slideIndicatorsNav.setAttribute('aria-label', placeholders.carouselSlideControls || 'Carousel Slide Controls');
+      slideIndicators = document.createElement('ol');
+      slideIndicators.classList.add('carousel-slide-indicators');
+      slideIndicatorsNav.append(slideIndicators);
+      block.append(slideIndicatorsNav);
 
-    const slideNavButtons = document.createElement('div');
-    slideNavButtons.classList.add('carousel-navigation-buttons');
-    slideNavButtons.innerHTML = `
-      <button type="button" class= "slide-prev" aria-label="${placeholders.previousSlide || 'Previous Slide'}"></button>
-      <button type="button" class="slide-next" aria-label="${placeholders.nextSlide || 'Next Slide'}"></button>
-    `;
+      const slideNavButtons = document.createElement('div');
+      slideNavButtons.classList.add('carousel-navigation-buttons');
+      slideNavButtons.innerHTML = `
+        <button type="button" class= "slide-prev" aria-label="${placeholders.previousSlide || 'Previous Slide'}"></button>
+        <button type="button" class="slide-next" aria-label="${placeholders.nextSlide || 'Next Slide'}"></button>
+      `;
 
-    container.append(slideNavButtons);
-  }
-
-  rows.forEach((row, idx) => {
-    const slide = createSlide(row, idx, carouselId);
-    slidesWrapper.append(slide);
-
-    if (slideIndicators) {
-      const indicator = document.createElement('li');
-      indicator.classList.add('carousel-slide-indicator');
-      indicator.dataset.targetSlide = idx;
-      indicator.innerHTML = `<button type="button"><span>${placeholders.showSlide || 'Show Slide'} ${idx + 1} ${placeholders.of || 'of'} ${rows.length}</span></button>`;
-      slideIndicators.append(indicator);
+      container.append(slideNavButtons);
     }
-    row.remove();
-  });
 
-  container.append(slidesWrapper);
-  block.prepend(container);
+    rows.forEach((row, idx) => {
+      const slide = createSlide(row, idx, carouselId);
+      slidesWrapper.append(slide);
 
-  if (!isSingleSlide) {
-    bindEvents(block);
-    startAutoScroll(block);
-  }
-}
+      if (slideIndicators) {
+        const indicator = document.createElement('li');
+        indicator.classList.add('carousel-slide-indicator');
+        indicator.dataset.targetSlide = idx;
+        indicator.innerHTML = `<button type="button"><span>${placeholders.showSlide || 'Show Slide'} ${idx + 1} ${placeholders.of || 'of'} ${rows.length}</span></button>`;
+        slideIndicators.append(indicator);
+      }
+      row.remove();
+    });
+
+    container.append(slidesWrapper);
+    block.prepend(container);
+
+    if (!isSingleSlide) {
+      bindEvents(block);
+      startAutoScroll(block);
+    }
+ }
 }
