636fc1d89840150b23775af600af94016d580577
Tracy Tucker <tracy.tucker@slalom.com>
2024-04-27 10:22:05 -0500

    Creates footer class names and button expansion

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,6 +1,55 @@
 import { getMetadata } from '../../scripts/aem.js';
 import { loadFragment } from '../fragment/fragment.js';
 
+function expandSection(block) {
+  if (block) {
+    requestAnimationFrame(() => {
+      const gray = block.querySelector('.gray');
+      if (gray) {
+        const button = gray.querySelector('.button');
+        if (button) {
+          const buttonContainer = button.parentElement;
+          const defaultContainer = buttonContainer.parentElement;
+          button.addEventListener('click', (e) => {
+            e.preventDefault();
+            defaultContainer.style.display = 'none';
+            gray.style.maxHeight = `${gray.scrollHeight}px`;
+          });
+        }
+      }
+    });
+  }
+}
+
+function createClassNames(block) {
+  if (block) {
+    requestAnimationFrame(() => {
+      const gray = block.querySelector('.gray');
+      if (gray) {
+        const columnsBlock = gray.querySelector('.columns');
+        if (columnsBlock) {
+          columnsBlock.querySelectorAll(':scope > div div').forEach((column0) => {
+            if (column0.querySelector('ul')) {
+              const level0 = column0.querySelector('ul');
+              level0.classList.add('main-column');
+              level0.querySelectorAll(':scope > li > ul').forEach((column1) => {
+                if (column1) {
+                  column1.classList.add('column-level-1');
+                  column1.querySelectorAll(':scope > li > ul').forEach((column2) => {
+                    if (column2) {
+                      column2.classList.add('column-level-2');
+                    }
+                  });
+                }
+              });
+            }
+          });
+        }
+      }
+    });
+  }
+}
+
 /**
  * loads and decorates the footer
  * @param {Element} block The footer block element
@@ -16,5 +65,7 @@ export default async function decorate(block) {
   const footer = document.createElement('div');
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
 
+  createClassNames(block);
+  expandSection(block);
   block.append(footer);
 }
